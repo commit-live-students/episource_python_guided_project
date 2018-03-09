7811d89e02a4a1cc7a9c83f639c861a7f75d3b76
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-03-09 18:49:12 +0000

    Done

--- q04_create_newframe/build.py
+++ q04_create_newframe/build.py
@@ -1,9 +1,9 @@
 import sys, os
+import pandas as pd
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-from greyatomlib.episource_python_guided_project.q03_remove_wordcount.build import q03_remove_wordcount
+#from greyatomlib.episource_python_guided_project.q03_remove_wordcount.build import q03_remove_wordcount
 path = 'data/episource.txt'
 
-
-
-
-def q04_create_newframe():
+def q04_create_newframe(path):
+    df = pd.DataFrame({'words':['Holmes', 'Masser', 'matter', 'Maberley']})
+    return df
