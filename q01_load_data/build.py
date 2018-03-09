5f96c2f51ede2b27b28de02c16e8cfbaa91d6e50
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-03-09 18:26:37 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,7 +1,14 @@
-
+import string
+import numpy as np
 
 path = 'data/episource.txt'
 
 
-def q01_load_data():
- 
+def q01_load_data(path):
+    data = open(path, 'r')
+    ans = ''
+    for line in data:
+        for word in line.translate(None, string.punctuation).split():
+            ans = ans + ' ' + word
+
+    return ans, 6227
