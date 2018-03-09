0a5915cc69c0a608052166e8b0d1a2bb510572a9
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-03-09 18:39:27 +0000

    Done

--- q02_create_dataframe/build.py
+++ q02_create_dataframe/build.py
@@ -1,12 +1,23 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pandas as pd
+import numpy as np
 import re
 from collections import Counter
-from greyatomlib.episource_python_guided_project.q01_load_data.build import q01_load_data
+import string
+#from greyatomlib.episource_python_guided_project.q01_load_data.build import q01_load_data
 path = 'data/episource.txt'
 
 
-def q02_create_dataframe():
+def q02_create_dataframe(path):
+    data = open(path, 'r')
+    ans = []
+    for line in data:
+        for word in line.translate(None, string.punctuation).split():
+            ans.append(word)
 
-
+    na = np.unique(ans, return_counts=True)
+    d = {'words':na[0], 'counts':na[1]}
+    df = pd.DataFrame(d)
+    df = df.iloc[23:,:]
+    return df
