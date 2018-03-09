a25d4da72749b26c9bd59abc7450591088a99c4e
inivikrant <inivikrant@gmail.com>
2018-03-09 05:44:33 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,7 +1,8 @@
-
-
+import numpy as np
+import pandas as pd
 path = 'data/episource.txt'
-
-
-def q01_load_data():
- 
+def q01_load_data(path):
+    file = open(path, 'r')
+    data = file.read()
+    return data, len(data.split(" "))
+print(q01_load_data(path))
