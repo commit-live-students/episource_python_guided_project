5accb6e62f092805c432ff168c6cbe70470f3cc1
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-03-09 18:45:34 +0000

    Done

--- q03_remove_wordcount/build.py
+++ q03_remove_wordcount/build.py
@@ -1,7 +1,22 @@
 import sys, os
+import string
+import numpy as np
+import pandas as pd
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-from greyatomlib.episource_python_guided_project.q02_create_dataframe.build import q02_create_dataframe
+#from greyatomlib.episource_python_guided_project.q02_create_dataframe.build import q02_create_dataframe
 path = 'data/episource.txt'
 
 
-def q03_remove_wordcount():
+def q03_remove_wordcount(path):
+    data = open(path, 'r')
+    ans = []
+    for line in data:
+        for word in line.translate(None, string.punctuation).split():
+            ans.append(word)
+
+    na = np.unique(ans, return_counts=True)
+    d = {'words':na[0], 'counts':na[1]}
+    df = pd.DataFrame(d)
+    df1 = df[df['counts'] > 3]
+    df2 = df1.iloc[56:,:]
+    return df2
