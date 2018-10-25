958136d10d79ae1b026ca0f4c30bb916bd586d30
rianboy18 <pranavr95.pn@gmail.com>
2018-10-25 10:15:51 +0000

    Done

--- q03_remove_wordcount/build.py
+++ q03_remove_wordcount/build.py
@@ -1,7 +1,28 @@
+# %load q03_remove_wordcount/build.py
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from greyatomlib.episource_python_guided_project.q02_create_dataframe.build import q02_create_dataframe
 path = 'data/episource.txt'
+import re
+from collections import Counter
+import pandas as pd
+def q03_remove_wordcount(path):
+    file=open(path,'rt')
+    text=file.read()
+    file.close()
+    # text=text.split()
+    words=re.split('[^a-zA-Z]',text)
+    af = Counter(words)
+    df = list(af)
+    df = df[1:]
+    keys = list(af.keys())
+
+    values = list(af.values())
+    values
+    dff = pd.DataFrame({'words':keys[1:],'count':values[1:]})
+    return dff[dff['count']>=5]
+
+c=q03_remove_wordcount(path)
+c
 
 
-def q03_remove_wordcount():
