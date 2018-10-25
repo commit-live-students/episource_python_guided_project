2375b6e9e5031cb003f13497f2e019bb7d3a8ac1
ramyakrishnan86 <ramya_krishnan86@yahoo.co.in>
2018-10-25 17:16:11 +0000

    Done

--- q02_create_dataframe/build.py
+++ q02_create_dataframe/build.py
@@ -1,5 +1,6 @@
+# %load q02_create_dataframe/build.py
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pandas as pd
 import re
 from collections import Counter
@@ -7,6 +8,18 @@ from greyatomlib.episource_python_guided_project.q01_load_data.build import q01_
 path = 'data/episource.txt'
 
 
-def q02_create_dataframe():
+def q02_create_dataframe(data):
+# write your solution here'
+    data = q01_load_data(path)
+
+    regex = re.compile('[^a-zA-Z]')
+    regex_data = regex.sub(' ', data[0])
+    words = Counter(regex_data.split())
+
+    df = pd.DataFrame(list(words.items()), columns=['words', 'count'])
+    
+    return df
+
+# q02_create_dataframe(path)
 
 
--- q04_create_newframe/build.py
+++ q04_create_newframe/build.py
@@ -1,9 +1,18 @@
+# %load q04_create_newframe/build.py
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from greyatomlib.episource_python_guided_project.q03_remove_wordcount.build import q03_remove_wordcount
 path = 'data/episource.txt'
 
 
+def q04_create_newframe(path):
+
+    df = q03_remove_wordcount(path)
+    condition = (df['words'].str.len() > 5) & (df['count'] > 10)
+
+    conditional_df = df.loc[condition]
+    return conditional_df
+
+# q04_create_newframe(path)
 
 
-def q04_create_newframe():
