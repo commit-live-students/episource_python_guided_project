ab6e8f631a667c8e2db7e2cf9629c6bc06122a8f
ramyakrishnan86 <ramya_krishnan86@yahoo.co.in>
2018-10-25 16:44:49 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,7 +1,16 @@
+# %load q01_load_data/build.py
 
 
 path = 'data/episource.txt'
 
 
-def q01_load_data():
- 
+
+def q01_load_data(path):
+    file = open(path, 'r')
+    data = file.read()
+    length = len(data.split())
+    return data, length
+
+# q01_load_data(path)
+
+
