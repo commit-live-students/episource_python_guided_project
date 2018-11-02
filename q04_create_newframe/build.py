e95bc61714c7449a12a98fb60f5732384131c0b6
rajeshbrid <rajesh_brid@hotmail.com>
2018-11-02 04:33:18 +0000

    Done

--- q04_create_newframe/build.py
+++ q04_create_newframe/build.py
@@ -1,9 +1,17 @@
+# %load q04_create_newframe/build.py
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from greyatomlib.episource_python_guided_project.q03_remove_wordcount.build import q03_remove_wordcount
 path = 'data/episource.txt'
 
+def q04_create_newframe(path):
+    df = q03_remove_wordcount(path)
+    condition = (df['words'].str.len() > 5) & (df['count'] > 10)
+    condition_df = df.loc[condition]
+    # print (condition_df)
+    return condition_df
+
+# q04_create_newframe(path)
 
 
 
-def q04_create_newframe():
