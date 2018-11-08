e48d3933bcdfdfbe01b130607bd66eb6d666e691
sonik8494 <sourabh.niks1994@gmail.com>
2018-11-08 14:25:07 +0000

    Done

--- q02_create_dataframe/build.py
+++ q02_create_dataframe/build.py
@@ -1,12 +1,23 @@
+# %load q02_create_dataframe/build.py
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pandas as pd
+import numpy as np
 import re
 from collections import Counter
 from greyatomlib.episource_python_guided_project.q01_load_data.build import q01_load_data
 path = 'data/episource.txt'
 
-
-def q02_create_dataframe():
+def q02_create_dataframe(path):
+    data = q01_load_data(path)
+    a = re.sub(r'[^a-zA-Z]',' ', data[0])
+    b = Counter(a.split())
+    df = pd.DataFrame(list(b.items()),columns=['words','count'] )
+    return df
+# data = q01_load_data((path))
+# # regex = re.compile('[^a-zA-Z]')
+# # regex_data = regex.sub(' ', data[0])
+# # words = Counter(regex_data.split())
+q02_create_dataframe(path)
 
 
