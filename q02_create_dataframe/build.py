84ca4aaa1c0be64496146bebf39143ebb3601a85
EminentNeo <aniketjagtap18@gmail.com>
2018-11-21 10:10:15 +0000

    Done

--- q02_create_dataframe/build.py
+++ q02_create_dataframe/build.py
@@ -1,12 +1,19 @@
+# %load q02_create_dataframe/build.py
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pandas as pd
+import numpy as np
 import re
 from collections import Counter
 from greyatomlib.episource_python_guided_project.q01_load_data.build import q01_load_data
 path = 'data/episource.txt'
 
-
-def q02_create_dataframe():
+def q02_create_dataframe(path):
+    data = q01_load_data(path)
+    a = re.sub(r'[^a-zA-Z]',' ', data[0])
+    b = Counter(a.split())
+    df = pd.DataFrame(list(b.items()),columns=['words','count'] )
+    return df
+q02_create_dataframe(path)
 
 
