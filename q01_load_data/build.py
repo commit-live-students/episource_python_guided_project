c1c77db3b877da301938e4760c5b879c88432a3f
EminentNeo <aniketjagtap18@gmail.com>
2018-11-21 09:55:25 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,7 +1,21 @@
+# %load q01_load_data/build.py
 
 
 path = 'data/episource.txt'
 
 
-def q01_load_data():
+fname = path
  
+
+def q01_load_data(path):
+    num_words = 0
+    with open(path, 'r') as f:
+        for line in f:
+            words = line.split()
+            num_words += len(words)
+    return f,num_words
+q01_load_data(path)
+ 
+
+
+
